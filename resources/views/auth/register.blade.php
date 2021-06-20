b8359a74a8afae2397e256104ecba69c1b5802df
ashjeman <a.azman@live.iium.edu.my>
2021-06-20 15:44:36 +0800

    Category controller

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,6 +64,7 @@
                 <select name="role_id" class="block mt-1 w-full border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
                     <option value="customer">Customer</option>
                     <option value="supplier">Supplier</option>
+                    <option value="admin">Admin</option>
                 </select>
             </div>
 
